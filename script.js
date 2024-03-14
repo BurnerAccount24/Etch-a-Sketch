64e583678189287c16b56d1455d7e0982e5a23fc
Mariam <meem7255@gmail.com>
2024-03-14 08:24:23 +0300

    created the initial steps of the Etch-a-Sketch  project on odin.

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 let board = document.querySelector('.board');
+let userBtn = document.querySelector('button');
 
-for (let i = 0; i < 16 * 16; i++) {
-	let square = document.createElement('div');
-	board.appendChild(square);
+function gridFunc(val) {
+	for (let i = 0; i < val * val; i++) {
+		let square = document.createElement('div');
+		square.classList.add('squareElement');
+		board.appendChild(square);
+		hoverEffect(square);
+	}
+	console.log(`value of the square:`, val * val);
+}
+gridFunc(16);
+userBtn.addEventListener('click', () => {
+	userInput = Number(prompt('enter a number between 16 and 100'));
+	userGrid();
+	console.log(`value of the user input square:`, userInput);
+});
+function userGrid() {
+	let removeSquare = document.querySelectorAll('.squareElement');
+	board.remove(removeSquare);
+	gridFunc(userInput);
+}
+// let userInput = 64;
+// for (let i = 0; i < 16 * 16; i++) {
+// 	let square = document.createElement('div');
+// let size = 100 / userInput;
+// square.style.cssText = `width: ${size}%; height:${size}%`;
+// 	board.appendChild(square);
+// 	hoverEffect(square);
+// }
+
+function hoverEffect(square) {
+	square.addEventListener('mouseover', (e) => {
+		e.target.classList.add('active');
+	});
 }
